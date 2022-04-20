b1256a697667bc749951c650006d3b39476a0433
Sergey_Shteyman <Shteiman10@gmail.com>
2022-04-20 22:23:47 +0300

    start develop branch

--- Modules/CalendarPresenter.swift
+++ Modules/CalendarPresenter.swift
@@ -6,18 +6,28 @@
 //
 
 import Foundation
+import UIKit
 
 // MARK: - CalendarPresenterProtocol
 protocol CalendarPresenterProtocol: AnyObject {
     
 }
-
 // MARK: - CalendarPresenter
 final class CalendarPresenter {
     
     var viewController: CalendarViewController?
     
     private let moduleBuilder: Buildable
+    
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private lazy var weekDayStackView: UIStackView = {
+        let stackView = UIStackView()
+        return stackView
+    }()
     
     init(moduleBuilder: Buildable) {
         self.moduleBuilder = moduleBuilder
