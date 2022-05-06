1f06f7607eae1637e0c34c6a16cd730ac12f2c3b
Sergey_Shteyman <Shteiman10@gmail.com>
2022-05-07 00:06:30 +0300

    Setup UITextView. Implementation UITextViewDelegate

--- Modules/Task/TaskViewController.swift
+++ Modules/Task/TaskViewController.swift
@@ -22,7 +22,9 @@ final class TaskViewController: UIViewController {
         textField.font = .boldSystemFont(ofSize: 30)
         textField.textAlignment = .center
         textField.placeholder = "N A M E  T A S K"  // To Enum
-        textField.tintColor = .black
+        textField.textColor = .black
+//        textField.text = "N A M E  T A S K"
+        textField.tintColor = .red
         textField.keyboardType = .default
         textField.backgroundColor = .clear
         textField.delegate = self
@@ -37,6 +39,7 @@ final class TaskViewController: UIViewController {
         textField.textColor = .systemRed
         textField.font = .boldSystemFont(ofSize: 25)
         textField.layer.cornerRadius = 10
+//        textField.layer.borderColor = UIColor.lightGray.cgColor
         textField.layer.borderColor = UIColor.darkGray.cgColor
         return textField
     }()
@@ -54,12 +57,27 @@ final class TaskViewController: UIViewController {
     private lazy var label: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
-        label.font = .boldSystemFont(ofSize: 130)
+        label.font = .boldSystemFont(ofSize: 120)
         label.textAlignment = .center
-        label.text = "LET'S  MOVE  OUT"
+        label.text = "LET'S  MOVE"
         label.lineBreakMode = .byWordWrapping
         label.textColor = .white
         return label
+    }()
+    
+    private lazy var descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.tintColor = .red
+        textView.keyboardType = .default
+        textView.backgroundColor = .clear
+        textView.layer.borderWidth = 1
+        textView.layer.cornerRadius = 20
+        textView.textColor = .systemRed
+        textView.font = .boldSystemFont(ofSize: 30)
+        textView.textAlignment = .center
+        textView.text = "\nY O U R  \nD E S C R I P T I O N" //  To Enum
+        textView.delegate = self
+        return textView
     }()
  
     override func viewDidLoad() {
@@ -110,6 +128,28 @@ extension TaskViewController: UITextFieldDelegate {
     }
 }
 
+// MARK: - UITextViewDelegate Impl
+extension TaskViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if descriptionTextView.text == "\nY O U R  \nD E S C R I P T I O N" {
+            descriptionTextView.text = ""
+            descriptionTextView.textColor = .black
+            descriptionTextView.font = .boldSystemFont(ofSize: 20)
+            descriptionTextView.textAlignment = .left
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if descriptionTextView.text.isEmpty {
+            descriptionTextView.textColor = .systemRed
+            descriptionTextView.font = .boldSystemFont(ofSize: 30)
+            descriptionTextView.textAlignment = .center
+            descriptionTextView.text = "\nY O U R  \nD E S C R I P T I O N"
+        }
+    }
+}
+
 // MARK: Private Methods
 private extension TaskViewController {
     
@@ -139,7 +179,8 @@ private extension TaskViewController {
     }
     
     func addSubViews() {
-        let arraySubViews = [label, titleTextField, containerTextField]
+        let arraySubViews = [label, titleTextField, containerTextField,
+                             descriptionTextView]
         view.myAddSubViews(from: arraySubViews)
     }
     
@@ -148,7 +189,7 @@ private extension TaskViewController {
             label.topAnchor.constraint(equalTo: view.topAnchor),
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            label.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            label.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: 100),
             
             titleTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 70),
             titleTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
@@ -157,7 +198,12 @@ private extension TaskViewController {
             containerTextField.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 90),
             containerTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             containerTextField.widthAnchor.constraint(equalToConstant: 120),
-            containerTextField.heightAnchor.constraint(equalToConstant: 80)
+            containerTextField.heightAnchor.constraint(equalToConstant: 80),
+            
+            descriptionTextView.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 90),
+            descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            descriptionTextView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.bottomAnchor, constant: -30)
         ])
     }
 }
